eafc2646abe82fec1fa0de99e14f428aee61173a
PaulNdrei <andreipetrea3@gmail.com>
2022-09-19 13:16:20 +0200

    Add dependencies, extracted dir, update readme

--- README.md
+++ README.md
@@ -10,6 +10,18 @@ https://grobid.readthedocs.io
 java -jar grobid-core/build/libs/grobid-core-0.7.1-onejar.jar -gH /mnt/BSC/grobid-0.7.1/grobid-home/ -dIn /mnt/BSC/catala/corpora/tdxin -dOut /mnt/BSC/catala/corpora/tdxout -ignoreAssets -exe processFullText
 ```
 
+## Install and run
+
+### Virtual environment
+
+pdf2tei2txt was built and tested with Python3.9. It should work for Python >= 3.9 but it has not been tested with other versions than 3.9.
+
+For creating the virtual environment and installing the dependencies (from `requirements.txt`), run:
+
+```sh
+bash setup.sh
+```
+
 ### Help
 '''
 python tei2txt.py --help
@@ -21,16 +33,19 @@ Options:
 
   -h, --help            show this help message and exit
   
-  -i INPUT, --inputdir=INPUT
+  -i INPUT, --inputdir=INPUT [REQUIRED]
                         inputdirectory
                         
-  -o OUTPUT, --outputdirectory=OUTPUT
+  -o OUTPUT, --outputdirectory=OUTPUT [REQUIRED]
                         output directory
+
+  -e EXTRACTED --extractdirectory [OPTIONAL]
+                        tei-xml extracted directory
                         
-  -f FILTER, --filter=FILTER
+  -f FILTER, --filter=FILTER [OPTIONAL]
                         if filter by lang
                         
-  -s STATS, --stats=STATS
+  -s STATS, --stats=STATS [OPTIONAL]
                         lang_id stats in json format
  '''
 
--- tei2txt.py
+++ tei2txt.py
@@ -5,7 +5,9 @@ Created on Wed May  4 09:21:14 2022
 
 @author: carlos rodriguez (bsc)
 """
+import shutil
 from collections import Counter
+from pathlib import Path
 from bs4 import BeautifulSoup
 import langid
 import os, json, sys
@@ -19,9 +21,9 @@ def read_tei(tei_file):
     raise RuntimeError('Cannot generate a soup from the input')
 
 
-def extract_text(soup,p):
+def extract_text(soup, p):
     alltext = []
-    for data in soup.find_all(p): 
+    for data in soup.find_all(p):
         alltext.append(data.get_text())
     return alltext
 
@@ -33,7 +35,8 @@ def langdet(alltext):
             print(s, langid.classify(s)[0])
         else:
             langs.append(langid.classify(s)[0])
-    
+
+
 def langaverage(alltext):
     langs = []
     for s in alltext:
@@ -41,40 +44,48 @@ def langaverage(alltext):
     counts = Counter(langs)
     nca = counts['ca']
     nothers = sum([counts[x] for x in counts if (x != 'ca')])
-    cafr = (nca*100)/(nca+nothers)
+    cafr = (nca * 100) / (nca + nothers)
     return cafr
-    
-    
-    
-    
+
+
 def main():
     parser = OptionParser()
     parser.add_option("-i", "--inputdir", dest="input",
                       help="inputdirectory")
     parser.add_option("-o", "--outputdirectory", dest="output",
                       help="output directory")
+    parser.add_option("-e", "--extractdirectory", dest="extracted", help="extracted tei-xml directory")
+
     parser.add_option("-f", "--filter", dest="filter",
-                      help="if filter by lang", default=None)    
+                      help="if filter by lang", default=None)
     parser.add_option("-s", "--stats", dest="stats",
-                     help="lang_id stats", default=None)
-    
+                      help="lang_id stats", default=None)
+
     (options, args) = parser.parse_args(sys.argv)
-    if options.input.endswith("/"):
-        dirin = options.input
-    else:
-        dirin = options.input+"/"
-    if options.output.endswith("/"):
-         outdir = options.output
-    else: 
-        dirout = options.output+"/" 
+
+    input_dir = Path(options.input)
+    output_dir = Path(options.output)
+    extracted_dir = None
+
+    output_dir.mkdir(exist_ok=True)
+
+    if options.extracted:
+        extracted_dir = Path(options.extracted)
+        extracted_dir.mkdir(exist_ok=True)
+
+    # else:
+    #     extracted_dir = Path(f"{output_dir.parent}/extracted")
+
     if options.stats:
         stats = {}
-    all_files = os.listdir(dirin)
-    print("processing: "+str(len(all_files))+" files ...")
+    all_files = os.listdir(input_dir)
+    print(f"[TEI2TXT] Processing: {str(len(all_files))} files ...")
+    #
     for tei_file in all_files:
-        print(tei_file)
-        soup = read_tei(dirin+tei_file)
-        text_list = extract_text(soup,['head','p'])
+
+        print(Path(input_dir, tei_file))
+        soup = read_tei(Path(input_dir, tei_file))
+        text_list = extract_text(soup, ['head', 'p'])
         if text_list:
             if options.filter:
                 p = langaverage(text_list)
@@ -82,19 +93,22 @@ def main():
                     stats[tei_file] = p
                 if p > int(options.filter):
                     text_string = "\n".join(text_list)
-                    w = open(outdir+tei_file+".txt","w")
+                    w = open(output_dir + tei_file + ".txt", "w")
                     w.write(text_string)
                     w.close()
                 else:
                     pass
             else:
                 text_string = "\n".join(text_list)
-                w = open(outdir+tei_file+".txt","w")
+                w = open(Path(output_dir, f"{tei_file}.txt"), "w")
                 w.write(text_string)
                 w.close()
+            if extracted_dir is not None:
+                shutil.move(Path(input_dir, tei_file), extracted_dir)
     if options.stats:
-        with open("lang_id_stats.json","w") as jout:
-            json.dump(stats,jout,ensure_ascii=False,indent=1)
+        with open("lang_id_stats.json", "w") as jout:
+            json.dump(stats, jout, ensure_ascii=False, indent=1)
+
 
 if __name__ == "__main__":
     main()
